25bb1f837f60ce41302325796ca1bde7b4613a7b
artudor3 <artudor3@gmail.com>
2022-01-18 14:07:30 -0500

    finished js

--- js/site.js
+++ js/site.js
@@ -1,15 +1,50 @@
-// DISPLAY MESSAGE TO THE USER
-function getMessage() {
-    let inputMessage = document.getElementById("message").value;
+function getValues() {
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
 
-    Swal.fire({
-        icon: 'success',
-        title: 'Success!',
-        text: 'Your Message was "' + inputMessage + '"',
-        footer: '<a href="">Why do I have this issue?</a>'
-    });
+    startValue = parseInt(startValue);
+    endValue = parseInt(endValue);
 
-    console.log(inputMessage);
-    document.getElementById("message").value = "";
-    document.getElementById("message").focus();
+    // Check to see if values are integers
+    if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
+        //generate a list of numbers
+        let numbers = generateNumbers(startValue, endValue);
+
+        displayNumbers(numbers)
+
+    } else {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Only Integers are allowed for Hundo, please enter both values'
+        });
+    }
+}
+
+function generateNumbers(start, end){
+    let numbers = [];
+
+    for (let i = start; i <= end; i++){
+        numbers.push(i);
+        //console.log(i);
+    }
+
+    return numbers;
+}
+
+function displayNumbers(numArr){
+    let templateRows = '';
+    for (let i = 0; i < numArr.length; i++){
+        
+        if (i % 2 == 0 && i > 0){
+            className = "even";
+        } else {
+            className = "odd";
+        }
+        
+        let row = `<tr><td class="${className}">${i}</td></tr>`;
+        templateRows += row;
+    }
+    
+    document.getElementById("results").innerHTML = templateRows;
 }
